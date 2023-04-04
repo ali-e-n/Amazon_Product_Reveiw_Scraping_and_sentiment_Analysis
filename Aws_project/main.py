254d43750398147c9404c6355049e14a766d60d0
Ali Akbar khan <aliakbarkhan161@gmail.com>
2023-04-04 18:47:41 +0500

    Main.py edited==>Utf-8 encoder added

--- Aws_project/main.py
+++ Aws_project/main.py
@@ -13,7 +13,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 nltk.download('vader_lexicon')
-
+import re
 def scrape_reviews(product_url):
     # Open Chrome browser
     opts = Options()
@@ -33,24 +33,24 @@ def scrape_reviews(product_url):
     review_elems = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@data-hook='review']")))
     reviews = [elem.text for elem in review_elems]
     print(len(reviews))
-    # # Scroll down to load all reviews
     
+    # Scroll down to load all reviews
     while True:
         try:
             see_all_reviews = driver.find_element(By.XPATH,"//li[@class='a-last']/a")
             see_all_reviews.click()
             time.sleep(random.uniform(2.5, 4.9))
             # Scrape reviews
-            reviews1=[]
             review_elems1 = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@data-hook='review']")))
             reviews1 = [elem.text for elem in review_elems1]
             
             reviews.extend(reviews1)
-            print(reviews)
-    
+            
+            if len(reviews) >= 30:
+                break
         except NoSuchElementException:
             print('Nothing more to load')
-            continue
+            break
     print(len(reviews))   
 
     # Close browser
@@ -58,10 +58,16 @@ def scrape_reviews(product_url):
 
     return reviews
 
+
 def analyze_sentiments(reviews):
     # Initialize sentiment analyzer
     sid = SentimentIntensityAnalyzer()
     
+    
+    # reviews = [review.encode('utf-8') for review in reviews]
+    reviews = [review.lower().replace('\n', ' ').replace('\r', '') for review in reviews]
+    reviews = [' '.join(re.findall(r'\w+', review)) for review in reviews]
+
     # Analyze sentiments
     sentiments = []
     for review in reviews:
